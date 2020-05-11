dff518303d3064ed2775091662a862599d134108
Frederik Walther Liberoth Christoffersen <frederik210103@gmail.com>
2020-05-11 21:15:16 +0200

    Finished custom profile pictures function

--- app/funcs.py
+++ app/funcs.py
@@ -1,5 +1,8 @@
 from app import geolocator
 from geopy.exc import GeocoderTimedOut
+from PIL import Image
+import requests
+from io import BytesIO
 
 
 def geocode(address, attempt=1, max_attempts=5):
@@ -9,3 +12,7 @@ def geocode(address, attempt=1, max_attempts=5):
         if attempt <= max_attempts:
             return geocode(address, attempt=attempt + 1)
         raise
+
+
+def get_image_from(src):
+    return Image.open(BytesIO(requests.get(src).content))
